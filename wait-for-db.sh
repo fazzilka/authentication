b3ef9ffd946a6203314add57585c25f1c4b2dc65
Fazzil <fazzil349@gmail.com>
2025-10-31 13:43:50 +0300

    wait-for-db.sh

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -6,7 +6,7 @@ port="${DB_PORT:-5432}"
 user="${DB_USER:-postgres}"
 db="${DB_NAME:-app}"
 
-echo "⏳ Жду Postgres на $host:$port…"
+echo "Жду Postgres на $host:$port…"
 for i in {1..60}; do
   if PGPASSWORD="${DB_PASSWORD:-postgres}" \
      psql "host=$host port=$port user=$user dbname=$db" -c '\q' 2>/dev/null; then
